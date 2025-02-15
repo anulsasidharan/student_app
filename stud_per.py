28a0af43fb69c50d50df739e4aecaa83bfce7478
anulsasidharan <anulsasidharan@gmail.com>
2025-02-15 04:07:04 -0500

    database functionality updated.

--- stud_per.py
+++ stud_per.py
@@ -5,8 +5,8 @@ import pickle
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from mongodbconnect import uri
 
+uri = "mongodb+srv://anu:tiger@cluster0.57jxgvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
